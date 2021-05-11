8967694782a2552c7dd89ffcc4a6a696dc8a356b
Alyssa Aznim <alyssa.aznim@gmail.com>
2021-05-12 00:15:02 +0800

    jobs css done

--- src/containers/jobs/latestJobs.js
+++ src/containers/jobs/latestJobs.js
@@ -6,45 +6,61 @@ export default function LatestJobs() {
         <section className="jobContainer">
             <div className="jobTitle">Latest Jobs</div>
             <div className="jobList">
-                <div>
-                    <div>Duis aute irure dolor in reprehenderit</div>
-                    <div>$20000 - $30000 | LONDON </div>
-                    <p>
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamoo laboris nisi ut aliquip ex
-                        ea commodo consequat
-                    </p>
-                    <div>APPLY</div>
+                <div className="jobBox">
+                    <div className="jobCol">
+                        <div className="header1">Duis aute irure dolor in reprehenderit</div>
+                        <div>$20000 - $30000 | LONDON </div>
+                        <p>
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamoo laboris nisi ut aliquip ex
+                            ea commodo consequat
+                        </p>
+                    </div>
+                    <div className="btnApply">
+                        <div className="btnText">APPLY</div>
+                    </div>
                 </div>
-                <div>
-                    <div>Duis aute irure dolor in reprehenderit</div>
-                    <div>$20000 - $30000 | LONDON </div>
-                    <p>
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamoo laboris nisi ut aliquip ex
-                        ea commodo consequat
-                    </p>
-                    <div>APPLY</div>
+                <div className="jobBox">
+                    <div className="jobCol">
+                        <div className="header1">Duis aute irure dolor in reprehenderit</div>
+                        <div>$20000 - $30000 | LONDON </div>
+                        <p>
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamoo laboris nisi ut aliquip ex
+                            ea commodo consequat
+                        </p>
+                    </div>
+                    <div className="btnApply">
+                        <div className="btnText">APPLY</div>
+                    </div>
                 </div>
-                <div>
-                    <div>Duis aute irure dolor in reprehenderit</div>
-                    <div>$20000 - $30000 | LONDON </div>
-                    <p>
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamoo laboris nisi ut aliquip ex
-                        ea commodo consequat
-                    </p>
-                    <div>APPLY</div>
+                <div className="jobBox">
+                    <div className="jobCol">
+                        <div className="header1">Duis aute irure dolor in reprehenderit</div>
+                        <div>$20000 - $30000 | LONDON </div>
+                        <p>
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamoo laboris nisi ut aliquip ex
+                            ea commodo consequat
+                        </p>
+                    </div>
+                    <div className="btnApply">
+                        <div className="btnText">APPLY</div>
+                    </div>
                 </div>
-                <div>
-                    <div>Duis aute irure dolor in reprehenderit</div>
-                    <div>$20000 - $30000 | LONDON </div>
-                    <p>
-                        Ut enim ad minim veniam, quis nostrud
-                        exercitation ullamoo laboris nisi ut aliquip ex
-                        ea commodo consequat
-                    </p>
-                    <div>APPLY</div>
+                <div className="jobBox">
+                    <div className="jobCol">
+                        <div className="header1">Duis aute irure dolor in reprehenderit</div>
+                        <div>$20000 - $30000 | LONDON </div>
+                        <p>
+                            Ut enim ad minim veniam, quis nostrud
+                            exercitation ullamoo laboris nisi ut aliquip ex
+                            ea commodo consequat
+                        </p>
+                    </div>
+                    <div className="btnApply">
+                        <div className="btnText">APPLY</div>
+                    </div>
                 </div>
             </div>
         </section>
